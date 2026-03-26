874b3c21cc2914dc77ca3a1fe4cac55f0dbec4cc
PedroHenrique1605 <prodriguesxavier230@gmail.com>
2026-03-25 21:42:58 -0300

    Código do FornecedorRepositoryts

---
+++ src/repositories/FornecedorRepository.ts
@@ -0,0 +1,25 @@
+import db from '../database/database';
+import { Fornecedor } from '../models/Fornecedor';
+
+export class FornecedorRepository {
+    salvar(c: Fornecedor): Fornecedor {
+        const resultado = db
+            .prepare('INSERT INTO fornecedor (nome_empresa, cnpj) VALUES (?,?)')
+            .run(c.nome_empresa, c.cnpj);
+        
+        return { id: Number(resultado.lastInsertRowid),
+        nome_empresa: c.nome_empresa, cnpj: c.cnpj };
+}
+
+    listarForncedor(): Fornecedor[] {
+        return db.prepare('SELECT * FROM fornecedor').all() as Fornecedor[];
+ }
+    BuscarPorId(id: number): Fornecedor | null {
+        return (db.prepare('SELECT * FROM fornecedor WHERE id = ?').get(id) as Fornecedor) ??null;
+
+    }     
+
+    BuscarPorNome(nome: string): Fornecedor | null {
+        return  (db.prepare('SELECT * FROM fornecedor WHERE nome LIKE ?').get('%${nome}%') as Fornecedor) ??null;
+    }    
+}
